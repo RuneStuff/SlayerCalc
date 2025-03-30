6d0e2ba970cb0220516f72744c30045b59e35f3d
unknown <rtallarr@gmail.com>
2025-03-30 18:54:49 -0300

    feat: add analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Lit } from "litlyx-js"
 
 @Component({
     selector: 'app-root',
@@ -8,5 +9,8 @@ import { RouterOutlet } from '@angular/router';
     styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor() {
+    Lit.init("67e9b868b9698244376e56a1");
+  }
   title = 'TzTok-Bob';
 }
